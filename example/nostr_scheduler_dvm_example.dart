370f9e733d0b2bdd9c3904dedadc6a827078895b
Nogringo <npub1kg4sdvz3l4fr99n2jdz2vdxe2mpacva87hkdetv76ywacsfq5leqquw5te@uid.ovh>
2026-06-05 12:11:42 +0200

    first commit

---
+++ example/nostr_scheduler_dvm_example.dart
@@ -0,0 +1,6 @@
+import 'package:nostr_scheduler_dvm/nostr_scheduler_dvm.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/nostr_scheduler_dvm_test.dart
@@ -0,0 +1,16 @@
+import 'package:nostr_scheduler_dvm/nostr_scheduler_dvm.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
